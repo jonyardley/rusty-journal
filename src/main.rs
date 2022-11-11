29a6d060a34b2d7679ba40c485d53565a6d6f0d1
Jon Yardley <jonyardley@me.com>
2022-11-11 12:14:53 +0000

    Added better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+use anyhow::anyhow;
 mod cli;
 mod tasks;
-use std::path::PathBuf;
-
-use structopt::StructOpt;
-
 use cli::{Action::*, CommandLineArgs};
+use std::path::PathBuf;
+use structopt::StructOpt;
 use tasks::Task;
 
 fn find_default_journal_file() -> Option<PathBuf> {
@@ -14,7 +13,7 @@ fn find_default_journal_file() -> Option<PathBuf> {
     })
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let CommandLineArgs {
         action,
         journal_file,
@@ -22,12 +21,13 @@ fn main() {
 
     let journal_file = journal_file
         .or_else(find_default_journal_file)
-        .expect("Failed to load file!!!");
+        .ok_or(anyhow!("Failed to load Journal file."))?;
 
     match action {
         Add { task } => tasks::add_task(journal_file, Task::new(task)),
         List => tasks::list_tasks(journal_file),
         Done { position } => tasks::complete_task(journal_file, position),
-    }
-    .expect("Failed to perform action")
+    }?;
+
+    Ok(())
 }
