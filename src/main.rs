881bce5df42bae4976beeed1e314c5cf84178ed9
Jon Yardley <jonyardley@me.com>
2022-11-11 12:07:00 +0000

    Added list, add, adn remove inc. default journal file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
 mod cli;
+mod tasks;
+use std::path::PathBuf;
+
 use structopt::StructOpt;
 
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
+}
+
 fn main() {
-    cli::CommandLineArgs::from_args();
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .expect("Failed to load file!!!");
+
+    match action {
+        Add { task } => tasks::add_task(journal_file, Task::new(task)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }
+    .expect("Failed to perform action")
 }
